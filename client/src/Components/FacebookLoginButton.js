34c726f7f7a82d4f5d582bbb920a6bebac9bb963
Raphael Müller <raphaelplmuller@icloud.com>
2020-05-23 19:59:21 -0300

    Add facebook login button and sign in page

---
+++ client/src/Components/FacebookLoginButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FacebookLoginButton() {
+  return (
+    <form action={process.env.REACT_APP_SERVER_URL + 'auth/facebook'}>
+      <button type="submit">Entrar com Facebook</button>
+    </form>
+  );
+}
+
+export default FacebookLoginButton;
---
+++ client/src/Pages/SignIn/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FacebookLoginButton from 'Components/FacebookLoginButton'
+
+function SignIn() {
+  return (
+    <div className="sign-in-container">
+      <FacebookLoginButton></FacebookLoginButton>
+    </div>
+  )
+}
+
+export default SignIn
