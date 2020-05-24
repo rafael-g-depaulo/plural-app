2396845fe76685f263f95523439d8bcc2ea2e9f4
Raphael Müller <raphaelplmuller@icloud.com>
2020-05-24 17:22:55 -0300

    ✨ Update verify callback in passport config

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,9 +1,38 @@
 var FacebookStrategy = require("passport-facebook").Strategy;
+const User = require("../models/User");
 
-const verifyCallback = (token, refreshToken, profile, done) => {
+async function createUserFromProvider(facebookId) {
+  const user = await User.create({ facebook_id: facebookId });
+
+  console.log(`Created user: ${JSON.stringify(user)}`);
+
+  return user;
+}
+
+async function getUserByProviderId(facebookId) {
+  console.log(`\nSearching for user with id: ${facebookId}\n`);
+
+  const user = await User.findOne({ where: { facebook_id: facebookId } });
+
+  user == null
+    ? console.log("\nNo records found.\n")
+    : console.log(`\nFound the following user: ${JSON.stringify(user)}\n`);
+
+  return user;
+}
+
+const verifyCallback = async (token, refreshToken, profile, done) => {
   console.log(profile);
 
-  done(null, profile);
+  let user = await getUserByProviderId(profile.id);
+
+  if (user != null) {
+    return done(null, user);
+  }
+
+  user = await createUserFromProvider(profile.id);
+
+  return done(null, user);
 };
 
 module.exports = {
