22cafc2cca6083d234e3dfb13e3ae51e22ef03b4
Raphael Müller <raphaelplmuller@icloud.com>
2020-05-24 14:20:30 -0300

    ✏️ Fix form href url in facebook login button

--- client/src/Components/FacebookLoginButton.js
+++ client/src/Components/FacebookLoginButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function FacebookLoginButton() {
   return (
-    <form action={process.env.REACT_APP_SERVER_URL + 'auth/facebook'}>
+    <form action={process.env.REACT_APP_SERVER_URL + '/api/auth/facebook'}>
       <button type="submit">Entrar com Facebook</button>
     </form>
   );
