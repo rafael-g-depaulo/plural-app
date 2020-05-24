fe95a90939be9b4163c39ca4c512f405394e7637
Raphael Müller <raphaelplmuller@icloud.com>
2020-05-24 17:14:34 -0300

    🔧 Update database config and user migration

--- server/database/migrations/20200523235246-create-users.js
+++ server/database/migrations/20200523235246-create-users.js
@@ -3,8 +3,23 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable("users", {
-      id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
-      facebook_id: { type: Sequelize.INTEGER}
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      facebook_id: {
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
     });
   },
 
