e36b4fdb7502e08280fd0fc0d32e2e9aa15c4c03
Raphael Müller <raphaelplmuller@icloud.com>
2020-05-24 23:28:31 -0300

    🐛 Fix name of .env variables in Google strategy

--- server/config/passport.js
+++ server/config/passport.js
@@ -59,8 +59,8 @@ module.exports = {
     passport.use(
       new GoogleStrategy(
         {
-          clientID: process.env.GOOGLE_APP_ID,
-          clientSecret: process.env.GOOGLE_APP_SECRET,
+          clientID: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
           callbackURL: process.env.GOOGLE_CALLBACK_URL,
         },
         verifyCallback
