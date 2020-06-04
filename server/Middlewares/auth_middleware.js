437fd148604390bb2e78260d7d2342f6f1484582
Raphael Müller <raphaelplmuller@icloud.com>
2020-06-04 19:56:25 -0300

    Add token verifier in auth middleware

---
+++ server/Middlewares/auth_middleware.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = {
+  verifyToken(req, res, next) {
+    const token = req.cookies.token;
+
+    console.log(token);
+
+    if (token) {
+      jwt.verify(token, process.env.TOKEN_SECRET_KEY, function (err, decoded) {
+        if (err) {
+          console.log("\n[ERROR]", err.message);
+
+          return res.status(401).json({ error: "Unauthorized access." });
+        }
+
+        req.decoded = decoded;
+        next();
+      });
+    } else {
+      return res.status(403).json({ error: "No token provided." });
+    }
+  },
+};
