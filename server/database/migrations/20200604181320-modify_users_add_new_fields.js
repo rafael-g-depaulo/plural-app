b8b44b53119e638537f892c2be07ab243324386c
Raphael Müller <raphaelplmuller@icloud.com>
2020-06-04 16:06:43 -0300

    Add columns to user via migration

---
+++ server/database/migrations/20200604181320-modify_users_add_new_fields.js
@@ -0,0 +1,40 @@
+"use strict";
+
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return Promise.all([
+      queryInterface.addColumn("users", "email", {
+        type: Sequelize.STRING,
+        allowNull: true,
+      }),
+      queryInterface.addColumn("users", "password", {
+        type: Sequelize.STRING,
+        allowNull: true,
+      }),
+      queryInterface.addColumn("users", "phone_number", {
+        type: Sequelize.STRING,
+        allowNull: true,
+      }),
+      queryInterface.addColumn("users", "birthdate", {
+        type: Sequelize.DATE,
+      }),
+      queryInterface.addColumn("users", "name", {
+        type: Sequelize.STRING,
+      }),
+      queryInterface.addColumn("users", "city", {
+        type: Sequelize.STRING,
+      }),
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.removeColumn("users", "email"),
+      queryInterface.removeColumn("users", "password"),
+      queryInterface.removeColumn("users", "phone_number"),
+      queryInterface.removeColumn("users", "birthdate"),
+      queryInterface.removeColumn("users", "name"),
+      queryInterface.removeColumn("users", "city"),
+    ]);
+  },
+};
