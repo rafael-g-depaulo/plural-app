6e4cd131230ee1330019474137f44b87a4fd3192
Raphael Müller <raphaelplmuller@icloud.com>
2020-06-05 15:30:29 -0300

    Add user and auth sub-routers to main router

--- server/Routes/index.js
+++ server/Routes/index.js
@@ -1,17 +1,18 @@
-import express from "express"
-const PassportConfig = require("../config/passport")
+import express from "express";
 
-import GoogleRouter from "./GoogleRouter"
-import FacebookRouter from "./FacebookRouter"
+import GoogleRouter from "./GoogleRouter";
+import FacebookRouter from "./FacebookRouter";
 import BlogRouter from './BlogRouter'
 import EventRouter from './EventRouter'
-
-import User from '../models/User'
+import UserRouter from "./UserRouter";
+import AuthRouter from "./AuthRouter";
 
 // use dependency injection in module
 export default ({ passport }, config = { mergeParams: true }) => express.Router(config)
-  .use("/auth/facebook", FacebookRouter({ PassportConfig, passport }, config))
-  .use("/auth/google", GoogleRouter({ PassportConfig, passport }, config))
+  .use("/auth/facebook", FacebookRouter({ passport }, config))
+  .use("/auth/google", GoogleRouter({ passport }, config))
+  .use("/user", UserRouter({}, config))
+  .use("/auth", AuthRouter({}, config))
   .use(/\/blogs?/, BlogRouter({}, config))
   .use(/\/events?/, EventRouter({}, config))
   .use('/', MappingRouter({User}, config))
