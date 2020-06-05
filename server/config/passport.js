c34cd6f97436603a4c72910f28190d29d3005b4d
Raphael Müller <raphaelplmuller@icloud.com>
2020-06-05 15:30:06 -0300

    Add request param to verifyCallback

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,5 +1,6 @@
 var FacebookStrategy = require("passport-facebook").Strategy;
 var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const Utils = require("../utils");
 const User = require("../models/User");
 
 async function createUserFromProvider(providerId, provider) {
@@ -25,7 +26,7 @@ async function getUserByProviderId(providerId) {
   return user;
 }
 
-const verifyCallback = async (token, refreshToken, profile, done) => {
+const verifyCallback = async (req, token, refreshToken, profile, done) => {
   console.log(profile);
 
   const providerId = profile.id;
@@ -34,10 +35,20 @@ const verifyCallback = async (token, refreshToken, profile, done) => {
   let user = await getUserByProviderId(providerId);
 
   if (user != null) {
+    const userToken = await Utils.signToken(user.id, user.email);
+
+    req.token = userToken;
+
     return done(null, user);
   }
 
   user = await createUserFromProvider(providerId, provider);
+
+  //Check if user was created.
+
+  const userToken = await Utils.signToken(user.id, user.email);
+
+  req.token = userToken;
 
   return done(null, user);
 };
@@ -50,6 +61,7 @@ module.exports = {
           clientID: process.env.FACEBOOK_APP_ID,
           clientSecret: process.env.FACEBOOK_APP_SECRET,
           callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+          passReqToCallback: true,
         },
         verifyCallback
       )
@@ -62,9 +74,10 @@ module.exports = {
           clientID: process.env.GOOGLE_CLIENT_ID,
           clientSecret: process.env.GOOGLE_CLIENT_SECRET,
           callbackURL: process.env.GOOGLE_CALLBACK_URL,
+          passReqToCallback: true,
         },
         verifyCallback
       )
     );
-  }
+  },
 };
