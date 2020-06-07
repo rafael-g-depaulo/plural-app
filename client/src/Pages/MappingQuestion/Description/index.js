9fb50c2d25830dcac6d5b31c55be936566d45c13
clarice42 <claramaia820@gmail.com>
2020-06-07 14:23:06 -0300

    ✨Add description and button in MappingQuestionPage

--- client/src/Pages/MappingQuestion/Description/index.js
+++ client/src/Pages/MappingQuestion/Description/index.js
@@ -2,24 +2,33 @@ import React from "react";
 import styled from "styled-components";
 
 const Text = styled.h1`
-  width: 9.1em;
-  height: 8.4em;
-  font-family: Town80Text;
-  font-size: 2.6em;
-  font-weight: bold;
+  position: absolute;
+  width: 70vw;
+  height: 50vh;
+  font-family: Town Text;
+  font-size: 2em;
+  font-weight: 900;
   font-stretch: normal;
   font-style: normal;
   line-height: 0.91;
   letter-spacing: normal;
   text-align: justify;
   color: #ffffff;
-  position: absolute;
   text-transform: uppercase;
-  margin: 31.625vh 36.4vw 28.75vh 35.15vw;
+  margin: 34vh 12vw 16vh 18vw;
 
-  @media (max-width: 550px) {
-    font-size: 2em;
-    margin: 33.26vh 16.14vw 38.15vh 24.8vw;
+  @media (min-width: 1024px) {
+    width: 35vw;
+    height: 50vh;
+    font-size: 2.8em;
+    margin: 28vh 32vw 22vh 33vw;
+  }
+
+  @media (min-height: 800px) {
+    width: 35vw;
+    height: 50vh;
+    font-size: 3em;
+    margin: 32vh 32vw 24vh 33vw;
   }
 `;
 
--- client/src/Pages/MappingQuestion/index.js
+++ client/src/Pages/MappingQuestion/index.js
@@ -1,22 +1,38 @@
 import React from "react";
 import styled from "styled-components";
-import GostariadeParticipar from "./gostariadeparticipar_site_fundo.png"
+import GostariadeParticipar from "./gostariadeparticipar_site_fundo.png";
+import GostariadeParticipar_App from "./gostariadeparticipar_fundo.png";
 import Description from "./Description";
+import Button from "./Button";
 
 const Background = styled.div`
+  background-image: url(${GostariadeParticipar_App});
+  background-repeat: no-repeat;
+  background-position: center;
+  object-fit: cover;
+  background-size: cover;
+  width: 100vw;
+  min-height: 100vh;
+
+  @media (min-width: 1024px) {
     background-image: url(${GostariadeParticipar});
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;
-    width: 100vw;
-    height: 100vh;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export const MappingQuestion = ({ ...props }) => {
   return (
     <>
       <Background>
+        <Container>
           <Description />
+          <Button />
+        </Container>
       </Background>
     </>
   );
