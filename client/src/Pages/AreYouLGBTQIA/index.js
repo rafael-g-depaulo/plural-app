3d6d06cbfcb782b5f816dfcc680bfe0754d584fb
clarice42 <claramaia820@gmail.com>
2020-06-08 14:22:48 -0300

    📱 ✨ Add responsive components in AreYou page

--- client/src/Pages/AreYouLGBTQIA/index.js
+++ client/src/Pages/AreYouLGBTQIA/index.js
@@ -2,13 +2,30 @@ import React from "react";
 import styled from "styled-components";
 import Buttons from "./Buttons";
 import Question from "./Question";
+import AreYou_web from "./vc_eh_site_fundo.png";
+import AreYou_app from "./voce_eh_fundo.png";
+
+const Background = styled.div`
+background-image: url(${AreYou_app});
+  background-repeat: no-repeat;
+  background-position: center;
+  object-fit: cover;
+  background-size: cover;
+  width: 100vw;
+  min-height: 100vh;
+
+  @media (min-width: 900px) {
+    background-image: url(${AreYou_web});
+  }
+`;
 
 export const AreYou = ({ ...props }) => {
   return (
     <>
-      <h1>ola</h1>
-      <Question />
-      <Buttons />
+      <Background>
+        <Question />
+        <Buttons />
+      </Background>
     </>
   );
 };
