179f56c3c2f34b29bb253bd129618a26e3641955
clarice42 <claramaia820@gmail.com>
2020-06-08 14:22:48 -0300

    📱 ✨ Add responsive components in AreYou page

--- client/src/Pages/AreYouLGBTQIA/Buttons/index.js
+++ client/src/Pages/AreYouLGBTQIA/Buttons/index.js
@@ -1,8 +1,65 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-export const Buttons = ({ ...props}) => {
-    return (<></>);
-}
+const Container = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  width: 37.3vw;
+  height: 17.5vh;
+  margin: 62.5vh 31.3vw 20vh 31.3vw;
+
+  @media (min-width: 700px) {
+    width: 16.4vw;
+    height: 21.1vh;
+    margin: 63.6vh 41.7vw 20vh 41.9vw;
+  }
+`;
+
+const ButtonBox = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  width: 37.3vw;
+  height: 6vh;
+  border: solid 2px #fffdfd;
+  border-radius: 30px;
+  background-color: ${(props) =>
+    props.cover === true ? "#6739d1" : "#f58880"};
+  text-decoration: none;
+  font-family: Town Text;
+  font-size: 1.2em;
+  font-weight: 700;
+  text-transform: uppercase;
+  text-align: center;
+  color: #ffffff;
+
+  @media (min-width: 700px) {
+    width: 16.4vw;
+    height: 7vh;
+    border: solid 2px #fffdfd;
+    border-radius: 30px;
+    font-size: 1.7em;
+  }
+`;
+
+export const Buttons = ({ ...props }) => {
+  return (
+    <>
+      <Container>
+        <ButtonBox to="" cover={true}>
+          sim
+        </ButtonBox>
+        <ButtonBox to="" cover={false}>
+          não
+        </ButtonBox>
+      </Container>
+    </>
+  );
+};
 
 export default Buttons;
--- client/src/Pages/AreYouLGBTQIA/Question/index.js
+++ client/src/Pages/AreYouLGBTQIA/Question/index.js
@@ -1,8 +1,51 @@
 import React from "react";
 import styled from "styled-components";
 
-export const Question = ({ ...props}) => {
-    return (<></>);
-}
+const Container = styled.div`
+  position: absolute;
+  width: 64.3vw;
+  height: 13.6vh;
+  margin: 37.5vh 19.7vw 48.9vh 16vw;
+
+  @media (min-width: 800px) {
+    width: 28.3vw;
+    height: 21.2vh;
+    margin: 32vh 36.6vw 50.8vh 35.2vw;
+  }
+`;
+
+const Text = styled.h1`
+  font-family: Town Text;
+  font-size: 2.8em;
+  font-weight: 900;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 0.91;
+  letter-spacing: normal;
+  text-transform: uppercase;
+  text-align: justify;
+  color: #e73839;
+
+  span {
+    color: #f58880;
+  }
+
+  @media (min-width: 800px) {
+    font-size: 4em;
+  }
+`;
+
+export const Question = ({ ...props }) => {
+  return (
+    <>
+      <Container>
+        <Text>
+          você é <br /> <span>lgbtqia+?</span>
+        </Text>
+      </Container>
+    </>
+  );
+};
 
 export default Question;
+
