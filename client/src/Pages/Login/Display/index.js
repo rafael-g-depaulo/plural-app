59c439282b98aa8f1acf03b996812615a48b8640
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-06-11 12:11:38 -0300

    ♻️ Refactor responsiveness

--- client/src/Pages/Login/Display/index.js
+++ client/src/Pages/Login/Display/index.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+
+  height: 100%;
+
+  @media (max-width: 399px) {
+    /* styles para mobile */
+    background-color: #2f6f7a;
+  }
+  @media (min-width: 400px) {
+    /* styles para desktop */
+    background-color: #cf0f1a;
+  }
+`
+
+const Content = styled.div`
+  height: 100%;
+
+  min-width: 300px;
+  max-width: min(380px, 80%)
+`
 
 export const Display = ({
   ...props
 }) => {
   return (
-    <div></div>
+    <Container />
   )
 }
 
--- client/src/Pages/Login/Display/index.stories.js
+++ client/src/Pages/Login/Display/index.stories.js
@@ -2,18 +2,17 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 
 import styled from 'styled-components'
-import Desktop from './Desktop'
-import Mobile from './Mobile'
+import Display from '.'
 
-const DesktopDisplay = styled.div`
+const Desktop = styled.div`
   height: 800px;
   width: 1280px;
 `
-const MobileDisplay = styled.div`
+const Mobile = styled.div`
   height: 667px;
   width: 375px;
 `
 
 storiesOf("Login Page", module)
-  .add("Desktop", () => <DesktopDisplay><Desktop /></DesktopDisplay>)
-  .add("Mobile", () => <MobileDisplay><Mobile /></MobileDisplay>)
+  .add("Desktop", () => <Desktop><Display /></Desktop>)
+  .add("Mobile", () => <Mobile><Display /></Mobile>)
