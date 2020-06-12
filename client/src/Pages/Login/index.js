e9f88c84b5ef3e66683485b444d22f6cea028950
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-06-12 12:50:44 -0300

    ✨ Add basic state handling to Login

--- client/src/Pages/Login/index.js
+++ client/src/Pages/Login/index.js
@@ -1,11 +1,48 @@
 import React from 'react'
 import Display from './Display'
+import { useState } from 'react'
+import { useCallback } from 'react'
 
 export const Login = ({
   ...props
 }) => {
+
+  // handle input
+  const [ email, setEmail ] = useState("")
+  const [ pwd, setPwd ] = useState("")
+  const onChangeEmail = useCallback(e => {
+    setEmail(e.target.value)
+  }, [])
+  const onChangePwd = useCallback(e => {
+    setPwd(e.target.value)
+  }, [])
+
+  // submit login form
+  const onSubmit = useCallback(e => {
+    e.preventDefault()
+    console.log(email, pwd)
+    // call API to log in
+  }, [email, pwd])
+
+  // favebook/google auths
+  const facebookLogin = useCallback(e => {
+    // call API to log in
+  }, [])
+  const googleLogin = useCallback(e => {
+    // call API to log in
+  }, [])
+
   return (
     <Display
+      {...{
+        onSubmit,
+        email,
+        onChangeEmail,
+        pwd,
+        onChangePwd,
+        facebookLogin,
+        googleLogin,
+      }}
       {...props}
     />
   )
