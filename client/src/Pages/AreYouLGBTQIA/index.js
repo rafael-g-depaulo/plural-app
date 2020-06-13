54fd175123cab76512aefe0d70b49d16b1913358
clarice42 <claramaia820@gmail.com>
2020-06-13 18:25:51 -0300

    🍱 📱 🎨  Adjust component's responsiveness
    Create new component Background and add global component Logo

--- client/src/Pages/AreYouLGBTQIA/index.js
+++ client/src/Pages/AreYouLGBTQIA/index.js
@@ -1,31 +1,16 @@
 import React from "react";
-import styled from "styled-components";
+import Background from "./Background";
 import Buttons from "./Buttons";
+import Logo from "./Logo";
 import Question from "./Question";
-import AreYou_web from "./vc_eh_site_fundo.png";
-import AreYou_app from "./voce_eh_fundo.png";
-
-const Background = styled.div`
-background-image: url(${AreYou_app});
-  background-repeat: no-repeat;
-  background-position: center;
-  object-fit: cover;
-  background-size: cover;
-  width: 100vw;
-  min-height: 100vh;
-
-  @media (min-width: 900px) {
-    background-image: url(${AreYou_web});
-  }
-`;
 
 export const AreYou = ({ ...props }) => {
   return (
     <>
-      <Background>
-        <Question />
-        <Buttons />
-      </Background>
+      <Background />
+      <Logo />
+      <Question />
+      <Buttons />
     </>
   );
 };
