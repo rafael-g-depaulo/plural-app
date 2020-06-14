6f5a4c1e345849dbcad13fdb372f64824cf2054c
Raphael Müller <raphaelplmuller@icloud.com>
2020-06-14 16:19:04 -0300

    Add route for email verification

--- server/Routes/UserRouter/index.js
+++ server/Routes/UserRouter/index.js
@@ -2,5 +2,5 @@ import { Router } from "express";
 const UsersController = require("../../controllers/users_controller");
 
 export default ({}, options) => {
-  return Router(options).post("/register", UsersController.create);
+  return Router(options).post("/register", UsersController.create).get("/verify-email/:token", UsersController.verifyEmail);
 };
--- server/utils/index.js
+++ server/utils/index.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 
 module.exports = {
   signToken(id, email) {
@@ -9,5 +10,37 @@ module.exports = {
     );
 
     return token;
+  },
+  sendConfirmationEmail(user, token) {
+    const transporter = nodemailer.createTransport({
+      service: "Sendgrid",
+      auth: {
+        user: process.env.SENDGRID_USERNAME,
+        pass: process.env.SENDGRID_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: user.email,
+      subject: "Verifique sua conta no Plural!",
+      text:
+        "Olá,\n\n" +
+        "Para utilizar o app Plural, você deve confirmar o seu email em: \n" + 
+        process.env.SERVER_URL +
+        "api/user/verify-email/" +
+        token +
+        "\n",
+    };
+    transporter.sendMail(mailOptions, function (err) {
+      if (err) {
+        console.log('Error when sending email:', err)
+        
+        return false;
+      }
+
+      console.log("Confirmation email sent!")
+      return true;
+    });
   },
 };
