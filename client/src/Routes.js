1cb9d9667c51087035f8b7d27307f218a008c5aa
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-06-14 22:08:06 -0300

    🐛 Fix routes Rafael broke during code review

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Mapping from "./Pages/Mapping";
+import Home from "Pages/Home";
 
 export const Routes = ({ ...props }) => {
   return (
     <Router basename="/">
       <Switch>
-        <Route path="/mapping" component={Mapping}></Route>
+        <Route path="/"><Home /></Route>
       </Switch>
     </Router>
   );
