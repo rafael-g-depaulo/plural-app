9c0c25e5c51dac278b68c308e689c23fc62241aa
Gabriel dos Santos Martins <gsmartins96@hotmail.com>
2020-06-16 15:12:14 -0300

    🔥 Return home route and remove components inpujob

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-// import Home from "Pages/Home";
+import Home from "Pages/Home";
 
-import Mapping from "Components/JobsInput/";
+// Rota para testar os inputs
+// import Mapping from "Components/JobsInput/";
 
 export const Routes = ({ ...props }) => {
   return (
     <Router basename="/">
       <Switch>
         <Route path="/">
-          <Mapping />
+          <Home />
         </Route>
       </Switch>
     </Router>
