be740c08c5e2ded5d0d02f8ca76cb1fcc9f1366f
Gabriel dos Santos Martins <gsmartins96@hotmail.com>
2020-06-16 15:07:51 -0300

    ⚡️ Improve input job

--- client/src/Components/JobsInput/index.js
+++ client/src/Components/JobsInput/index.js
@@ -3,39 +3,31 @@ import styled from "styled-components";
 
 import ChipInput from "material-ui-chip-input";
 
-import InputLabel from "./InputLabel";
-
 const Group = styled.div`
   display: flex;
   flex-direction: column;
   padding-bottom: 20px;
 `;
 
-export default function Mapping({ setJobs }) {
+export default function Mapping() {
   const [newJob, setNewJob] = useState();
 
-  // whenever chipData updates, set jobs
-  // se deixar setando os jobs sempre, ele vai pro input e fica uma bagunça
-  useEffect(() => {}, [setJobs]);
-
   const handleDelete = (chipToDelete) => () => {
+    console.log(newJob);
     setNewJob((chips) => chips.filter((chip) => chip.key !== chipToDelete.key));
   };
 
   function handleSubmit(chip) {
-    console.log("HANDLE SUBMIT");
-    setNewJob([...newJob], chip);
+    setNewJob(chip);
     console.log(newJob);
   }
 
   return (
     <>
       <Group>
-        <InputLabel>Insira seus trabalhos</InputLabel>
         <ChipInput
-          value={newJob}
-          onAdd={(chip) => handleSubmit(chip)}
-          onDelete={(chip) => handleDelete(chip)}
+          onChange={(chips) => handleSubmit(chips)}
+          onDelete={(newJob) => handleDelete(newJob)}
           newChipKeyCodes={[32, 13]}
         />
       </Group>
--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Home from "Pages/Home";
+// import Home from "Pages/Home";
 
 import Mapping from "Components/JobsInput/";
 
