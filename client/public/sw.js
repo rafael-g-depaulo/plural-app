e0b93dcbd165cf79484c6b14f79056dbd1b0ae95
Gabriel dos Santos Martins <gsmartins96@hotmail.com>
2020-06-17 16:10:30 -0300

    🚧 Trabalhando no PWA ainda

---
+++ client/public/sw.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-restricted-globals */
+var CACHE_NAME = "plural-app";
+
+var urlsToCache = ["/login"];
+
+self.addEventListener("install", function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
