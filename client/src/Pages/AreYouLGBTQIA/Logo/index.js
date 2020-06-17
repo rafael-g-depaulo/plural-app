a559eb3742e9f8eb7d5e14ee82c7ea782b1f03be
clarice42 <claramaia820@gmail.com>
2020-06-16 21:03:16 -0300

    ♻️ 💄 Fix buttons and delete scroll

--- client/src/Pages/AreYouLGBTQIA/Logo/index.js
+++ client/src/Pages/AreYouLGBTQIA/Logo/index.js
@@ -4,27 +4,31 @@ import PluralLogo from "../../../Components/Logo/plural_logo.png";
 
 const Container = styled.img`
   position: absolute;
+  object-fit: contain;
+  height: auto;
 
   @media (max-width: 599px) {
     width: 54.8vw;
-    height: 9.4vh;
-    margin: 22.2vh 29.2vw 68.4vh 16vw;
+    margin-top: 22.2vh;
+    margin-left: 16vw;
   }
 
   @media (min-width: 600px) and (max-width: 1023px) {
-    width: 35vw;
-    height: 9.1vh;
-    margin: 18vh 43vw 72.9vh 24vw;
+    width: 36vw;
+    margin-top: 18vh;
+    margin-left: 32vw;
   }
 
   @media (min-width: 1024px) {
-    width: 20.6vw;
-    height: 10.6vh;
-    margin: 17.4vh 44.2vw 72vh 35.2vw;
+    width: 19vw;
+    margin-top: 12.4vh;
+    margin-left: 40.5vw;
+  }
 
-    @media (min-height: 650px) {
-      height: 10.1vh;
-    }
+  @media (min-width: 1300px) and (min-height: 700px) {
+    width: 21vw;
+    margin-top: 13.4vh;
+    margin-left: 39.5vw;
   }
 `;
 
