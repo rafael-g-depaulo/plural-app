a5eaf40f63a54a288a95db939588398689e91b28
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-06-17 16:16:34 -0300

    👌 Add a callback to Mapping, and add other hooks

--- client/src/Components/JobsInput/index.js
+++ client/src/Components/JobsInput/index.js
@@ -1,23 +1,29 @@
-import React, { useState } from "react";
-import ChipInput from "material-ui-chip-input";
+import React, { useState, useEffect, useCallback } from "react"
+import ChipInput from "material-ui-chip-input"
 
-export default function Mapping() {
-  const [newJob, setNewJob] = useState();
+export default function Mapping({
+  onJobsChange = () => {},
+  ...props
+}) {
+  const [ newJob, setNewJob ] = useState([])
 
-  const handleDelete = (chipToDelete) => () => {
-    setNewJob((chips) => chips.filter((chip) => chip.key !== chipToDelete.key));
-  };
+  // update parent when jobs change
+  useEffect(() => { onJobsChange(newJob) }, [ newJob, onJobsChange ])
 
-  function handleSubmit(chip) {
-    setNewJob(chip);
-    console.log("Lista de Jobs:", newJob);
-  }
+  const handleDeleteJob = useCallback((chipToDelete) => () => {
+    setNewJob((chips) => chips.filter((chip) => chip.key !== chipToDelete.key))
+  }, [])
+
+  const handleNewJob = useCallback((chip) => {
+    setNewJob(chip)
+  }, [])
 
   return (
     <ChipInput
-      onChange={(chips) => handleSubmit(chips)}
-      onDelete={(chips) => handleDelete(chips)}
+      onChange={chips => handleNewJob(chips)}
+      onDelete={chips => handleDeleteJob(chips)}
       newChipKeyCodes={[32, 13]}
+      {...props}
     />
-  );
+  )
 }
