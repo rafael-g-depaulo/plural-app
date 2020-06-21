526480b27495f50f701a662c1edcf62da634538d
Raphael Müller <raphaelplmuller@icloud.com>
2020-06-20 21:13:53 -0300

    Add method to get current user

--- server/Routes/UserRouter/index.js
+++ server/Routes/UserRouter/index.js
@@ -1,6 +1,13 @@
 import { Router } from "express";
 const UsersController = require("../../controllers/users_controller");
 
-export default ({}, options) => {
-  return Router(options).post("/register", UsersController.create).get("/verify-email/:token", UsersController.verifyEmail);
+export default ({ AuthMiddleware }, options) => {
+  return Router(options)
+    .post("/register", UsersController.create)
+    .get("/verify-email/:token", UsersController.verifyEmail)
+    .get(
+      "/current",
+      AuthMiddleware.verifyToken,
+      UsersController.showCurrentUser
+    );
 };
--- server/Routes/index.js
+++ server/Routes/index.js
@@ -4,6 +4,7 @@ import FacebookRouter from "./FacebookRouter";
 import UserRouter from "./UserRouter";
 import AuthRouter from "./AuthRouter";
 import MappingRouter from "./mapping";
+import AuthMiddleware from "../Middlewares/auth_middleware";
 import User from "../models/User";
 
 // use dependency injection in module
@@ -12,6 +13,6 @@ export default ({ passport }, config = { mergeParams: true }) =>
     .Router(config)
     .use("/auth/facebook", FacebookRouter({ passport }, config))
     .use("/auth/google", GoogleRouter({ passport }, config))
-    .use("/user", UserRouter({}, config))
+    .use("/user", UserRouter({AuthMiddleware}, config))
     .use("/auth", AuthRouter({}, config))
     .use("/", MappingRouter({ User }, config));
