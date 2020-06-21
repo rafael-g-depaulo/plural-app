192b405cb6411237d408845abccc19160cd1bf69
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-06-21 17:34:33 -0300

    ♻️ Clean up code in server routing

--- server/Routes/index.js
+++ server/Routes/index.js
@@ -1,19 +1,17 @@
-import express from "express";
+import { Router } from "express"
 
-import GoogleRouter from "./GoogleRouter";
-import FacebookRouter from "./FacebookRouter";
+import GoogleRouter from "./GoogleRouter"
+import FacebookRouter from "./FacebookRouter"
 import BlogRouter from './BlogRouter'
 import EventRouter from './EventRouter'
-import UserRouter from "./UserRouter";
-import AuthRouter from "./AuthRouter";
-import MappingRouter from "./MappingRouter";
+import UserRouter from "./UserRouter"
+import AuthRouter from "./AuthRouter"
+import MappingRouter from "./MappingRouter"
 
-import User from "models/User";
-
-import AuthMiddleware from "Middlewares/auth_middleware"
+import User from "models/User"
 
 // use dependency injection in module
-export default ({ passport }, config = { mergeParams: true }) => express.Router(config)
+export default ({ passport }, config = { mergeParams: true }) => Router(config)
   .use("/auth/facebook", FacebookRouter({ passport }, config))
   .use("/auth/google", GoogleRouter({ passport }, config))
   .use("/user", UserRouter({}, config))
