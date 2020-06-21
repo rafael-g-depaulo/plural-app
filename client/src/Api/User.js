7814ddfea4ae4813564321845cbd6be7d7fb6910
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-06-21 17:32:50 -0300

    ✨ Add user API calling logic

---
+++ client/src/Api/User.js
@@ -0,0 +1,20 @@
+import api from 'Api'
+
+export const getCurrentUser = () => api
+  .get("/api/user/current", { withCredentials: true })
+  // .then(({ data }) => data.current_user)
+
+export const loginUser = ({ email, password }) => api
+  .post("/api/auth/login", { email, password })
+  // .then(({ data }) => data)
+
+export const registerUser = ({
+  email,
+  password,
+  city,
+  phoneNumber,
+  name,
+  birthdate,
+}) => api
+  .post("/api/user/register", { email, password, city, phoneNumber, name, birthdate, })
+  // .then(({ data }) => data)
