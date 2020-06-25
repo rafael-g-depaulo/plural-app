0470e64b867fc2b79c4c58439529c07dbac7dbba
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-06-25 15:38:04 -0300

    🐛 Fix useAPI

--- client/src/Hooks/useAPI.js
+++ client/src/Hooks/useAPI.js
@@ -1,16 +1,15 @@
 import { useEffect, useState } from "react"
 
-export const useAPI = (defaultValue, apiCall = Promise.resolve) => {
-  const [ response, setResponse ] = useState({ data: defaultValue, status: null, isFetching: true })
+export const useAPI = (apiCall = Promise.resolve) => {
+  const [ response, setResponse ] = useState({ data: null, status: null, isFetching: true })
 
   useEffect(() => {
     apiCall()
       .then(({ data, status }) => setResponse({ data, status, isFetching: false }))
-      .catch(err => {
-        console.log("2222",{err})
-        setResponse(r => ({ ...r, status: err.response.status, isFetching: false }))
-      })
+      .catch(({ response }) => setResponse({ data: null, status: response.status, isFetching: false }))
   }, [ apiCall ])
 
   return response
 }
+
+export default useAPI
