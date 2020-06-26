de782071f287e8dff778a0a284a2244bbe3e7280
Raphael Müller <raphaelplmuller@icloud.com>
2020-06-26 13:19:50 -0300

    Add password reset mail

--- server/utils/index.js
+++ server/utils/index.js
@@ -11,6 +11,15 @@ module.exports = {
 
     return token;
   },
+  signPasswordResetToken(id, email) {
+    const token = jwt.sign(
+      { user: { id, email } },
+      process.env.PASSWORD_RESET_TOKEN_SECRET_KEY,
+      { expiresIn: parseInt(process.env.PASSWORD_RESET_TOKEN_EXPIRATION_TIME) }
+    );
+
+    return token;
+  },
   sendConfirmationEmail(user, token) {
     const transporter = nodemailer.createTransport({
       service: "Sendgrid",
@@ -26,20 +35,54 @@ module.exports = {
       subject: "Verifique sua conta no Plural!",
       text:
         "Olá,\n\n" +
-        "Para utilizar o app Plural, você deve confirmar o seu email em: \n" + 
+        "Para utilizar o app Plural, você deve confirmar o seu email em: \n" +
         process.env.SERVER_URL +
         "api/user/verify-email/" +
         token +
         "\n",
     };
-    transporter.sendMail(mailOptions, function (err) {
+    transporter.sendMail(mailOptions, function(err) {
       if (err) {
-        console.log('Error when sending email:', err)
-        
+        console.log("Error when sending email:", err);
+
         return false;
       }
 
-      console.log("Confirmation email sent!")
+      console.log("Confirmation email sent!");
+      return true;
+    });
+  },
+  async sendPasswordResetEmail(user, token) {
+    const transporter = nodemailer.createTransport({
+      service: "Sendgrid",
+      auth: {
+        user: process.env.SENDGRID_USERNAME,
+        pass: process.env.SENDGRID_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: user.email,
+      subject: "Alteração de senha - App Plural",
+      text:
+        "Olá,\n\n" +
+        "Para redefinir sua senha, acesse o seguinte link: \n" +
+        process.env.CLIENT_URL +
+        "password-reset/" +
+        token +
+        "\n",
+    };
+
+    await transporter.sendMail(mailOptions, function(err) {
+      if (err) {
+        console.log("Error when sending email:", err);
+
+        return false;
+      }
+
+      console.log("Password reset email sent!");
+
       return true;
     });
   },
