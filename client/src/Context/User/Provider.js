36fc50f27896903bca30dccce21e0c9f90c06566
Raphael Müller <raphaelplmuller@icloud.com>
2020-06-27 18:36:34 -0300

    Add initial logic for user routing

--- client/src/Context/User/Provider.js
+++ client/src/Context/User/Provider.js
@@ -5,15 +5,40 @@ import { getCurrentUser } from "Api/User";
 export function UserProvider(props) {
   const [isLoading, setIsLoading] = useState(true);
 
-  const [currentUser, setCurrentUser] = useState(null);
+  const [currentUser, setCurrentUser] = useState(undefined);
 
   const [error, setError] = useState(null);
+
+  /*
+    Each time the current user gets updated, check if:
+      - Is active
+      - Is LGBTQ
+      - Has filled the mapping form 
+  */
+
+  useEffect(() => {
+    if (currentUser !== undefined) {
+      console.log("Current user has been updated", currentUser);
+
+      if (currentUser.active === false) {
+        console.log("Redirecting to email confirmation.");
+      } else if (currentUser.isLgbtq === null) {
+        console.log("Redirecting to is LGBTQ+?");
+      } else if (currentUser.isLgbtq === true && currentUser.mapping === null) {
+        console.log("Redirecting to mapping.");
+      } else {
+        console.log("Redirecting to homepage");
+      }
+    }
+  }, [currentUser]);
 
   useEffect(() => {
     let didCancel = false;
 
     async function fetchUserData() {
       !didCancel && setIsLoading(true);
+
+      console.log("Fetching current user...");
 
       try {
         const response = await getCurrentUser();
@@ -43,12 +68,14 @@ export function UserProvider(props) {
     };
   }, []);
 
-  if (error) console.warn("UserProvider Error:", error)
+  if (error) console.warn("UserProvider Error:", error);
 
   return isLoading === true ? (
     <h1>Loading</h1>
   ) : (
-    <UserContext.Provider value={currentUser}>
+    <UserContext.Provider
+      value={{ currentUser, setCurrentUser: (user) => setCurrentUser(user) }}
+    >
       {isLoading === true ? <h1>Loading...</h1> : props.children}
     </UserContext.Provider>
   );
