c46f90a24ff1ea99e736d220394151cc39a48e94
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-07-01 13:01:27 -0300

    🔥 Remove old way of defining fonts

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,11 @@ import React from 'react'
 
 import { UserProvider } from "Context/User/Provider"
 import GlobalStyles from 'GlobalStyles'
-import Fonts from 'Fonts'
 import Routes from 'Routes'
 
 const App = () => { 
   return (
     <>
-      <Fonts />
       <GlobalStyles />
       <UserProvider>
         <Routes />
