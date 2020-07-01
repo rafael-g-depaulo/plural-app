bc41a29f83bbe2898b113129a79152ed59bd9ba0
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-07-01 18:59:16 -0300

    ♻️ Remove public folder fonts, use Fonts folder fonts

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import { UserProvider } from "Context/User/Provider"
 import GlobalStyles from 'GlobalStyles'
+import Fonts from 'Fonts'
 import Routes from 'Routes'
 
 const App = () => { 
   return (
     <>
+      <Fonts />
       <GlobalStyles />
       <UserProvider>
         <Routes />
