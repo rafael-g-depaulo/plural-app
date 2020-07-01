42fd0ff40aa30c41f154c213d62d20e06f806b13
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-07-01 12:39:28 -0300

    🚚 🍱 Rename font assets. change font definition

--- client/src/Fonts/Town/index.js
+++ client/src/Fonts/Town/index.js
@@ -1,35 +1,39 @@
 import { css } from 'styled-components'
 
-import TownDisplayBlackOtf      from './Jason Vandenberg - Town 10 Display Black [TheFontsMaster.com].otf'
-import TownDisplayBoldOtf       from './Jason Vandenberg - Town 10 Display Bold [TheFontsMaster.com].otf'
-import TownDisplayMediumOtf     from './Jason Vandenberg - Town 10 Display Medium [TheFontsMaster.com].otf'
-import TownDisplayRegularTtf    from './Jason Vandenberg - Town 10 Display Regular [TheFontsMaster.com].ttf'
-import TownDisplayLightTtf      from './Jason Vandenberg - Town 10 Display Light [TheFontsMaster.com].ttf'
-import TownDisplayThinTtf       from './Jason Vandenberg - Town 10 Display Thin [TheFontsMaster.com].ttf'
-import TownDisplayExtraThinTtf  from './Jason Vandenberg - Town 10 Display Extra Thin [TheFontsMaster.com].ttf'
-import TownDisplayHairlineTtf   from './Jason Vandenberg - Town 10 Display Hairline [TheFontsMaster.com].ttf'
+import TownDisplayBlackOtf      from './Jason-Vandenberg-Town-10-Display-Black.otf'
+import TownDisplayBoldOtf       from './Jason-Vandenberg-Town-10-Display-Bold.otf'
+import TownDisplayMediumOtf     from './Jason-Vandenberg-Town-10-Display-Medium.otf'
+import TownDisplayRegularTtf    from './Jason-Vandenberg-Town-10-Display-Regular.ttf'
+import TownDisplayLightTtf      from './Jason-Vandenberg-Town-10-Display-Light.ttf'
+import TownDisplayThinTtf       from './Jason-Vandenberg-Town-10-Display-Thin.ttf'
+import TownDisplayExtraThinTtf  from './Jason-Vandenberg-Town-10-Display-Extra-Thin.ttf'
+import TownDisplayHairlineTtf   from './Jason-Vandenberg-Town-10-Display-Hairline.ttf'
 
-import TownTextBoldTtf          from './Jason Vandenberg - Town 80 Text Bold [TheFontsMaster.com].ttf'
-import TownTextBookTtf          from './Jason Vandenberg - Town 80 Text Book [TheFontsMaster.com].ttf'
-import TownTextLightTtf         from './Jason Vandenberg - Town 80 Text Light [TheFontsMaster.com].ttf'
-import TownTextMediumTtf        from './Jason Vandenberg - Town 80 Text Medium [TheFontsMaster.com].ttf'
-import TownTextThinTtf          from './Jason Vandenberg - Town 80 Text Thin [TheFontsMaster.com].otf'
+import TownTextBoldTtf          from './Jason-Vandenberg-Town-80-Text-Bold.ttf'
+import TownTextBookTtf          from './Jason-Vandenberg-Town-80-Text-Book.ttf'
+import TownTextLightTtf         from './Jason-Vandenberg-Town-80-Text-Light.ttf'
+import TownTextMediumTtf        from './Jason-Vandenberg-Town-80-Text-Medium.ttf'
+import TownTextThinTtf          from './Jason-Vandenberg-Town-80-Text-Thin.otf'
 
-import TownTextBoldItalicTtf    from './Jason Vandenberg - Town 80 Text Bold Italic [TheFontsMaster.com].ttf'
-import TownTextBookItalicTtf    from './Jason Vandenberg - Town 80 Text Book Italic [TheFontsMaster.com].otf'
-import TownTextLightItalicTtf   from './Jason Vandenberg - Town 80 Text Light Italic [TheFontsMaster.com].otf'
-import TownTextMediumItalicTtf  from './Jason Vandenberg - Town 80 Text Medium Italic [TheFontsMaster.com].ttf'
-import TownTextThinItalicTtf    from './Jason Vandenberg - Town 80 Text Thin Italic [TheFontsMaster.com].otf'
+import TownTextBoldItalicTtf    from './Jason-Vandenberg-Town-80-Text-Bold-Italic.ttf'
+import TownTextBookItalicTtf    from './Jason-Vandenberg-Town-80-Text-Book-Italic.otf'
+import TownTextLightItalicTtf   from './Jason-Vandenberg-Town-80-Text-Light-Italic.otf'
+import TownTextMediumItalicTtf  from './Jason-Vandenberg-Town-80-Text-Medium-Italic.ttf'
+import TownTextThinItalicTtf    from './Jason-Vandenberg-Town-80-Text-Thin-Italic.otf'
 
-const defineFont = (fontName, locals, url, format, weight, style) => css`
-  @font-face {
-    font-family: ${fontName};
-    src: ${locals.map(local => `local('${local}'), `)}
-    url('${url}') format('${format ?? 'truetype'}');
-    font-weight: ${weight ?? 300};
-    font-style: ${style ?? 'normal'};
-  }
-`
+const defineFont = (fontName, locals, url, format, weight, style) => {
+  const font = `
+    @font-face {
+      font-family: '${fontName}';
+      src: ${locals.map(local => `local('${local}'), `).join("")}
+      url('${url}') format('${format ?? 'truetype'}');
+      font-weight: ${weight ?? 300};
+      font-style: ${style ?? 'normal'};
+    }
+  `
+
+  return css`${font}`
+}
 
 export default css`
   ${defineFont("Town Display", ["Town Display", "TownDisplay", "Town-Display"], TownDisplayBlackOtf,    "truetype", 900)}
