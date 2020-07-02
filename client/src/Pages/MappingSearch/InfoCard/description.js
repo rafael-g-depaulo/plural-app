bf8f566b24893e8883a4bca68a94b6d78a00eceb
clarice42 <claramaia820@gmail.com>
2020-07-02 20:14:36 -0300

    💄 Add styles to description

--- client/src/Pages/MappingSearch/InfoCard/description.js
+++ client/src/Pages/MappingSearch/InfoCard/description.js
@@ -1,29 +1,37 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-    width: 205.5px;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-start;
-    padding: 14px 28px 20px 14px;
+  position: absolute;
+  width: 205.5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding: 14px 28px 20px 14px;
 `;
 
 const Title = styled.h1`
-    max-width: 205.5px;
-    font-family: "Town Text";
-    
+  max-width: 205.5px;
+  font-family: "Town Text";
+  font-size: 19px;
+  font-weight: 700;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: justify;
+  text-transform: uppercase;
+  color: #000;
+  opacity: 1;
 `;
 
-export const description = ({
-    ...props
-}) => {
-    return (
-        <Container>
-
-        </Container>
-    )
-}
+export const description = ({ ...props }) => {
+  return (
+    <Container>
+      <Title>madamy</Title>
+    </Container>
+  );
+};
 
 export default description;
--- client/src/Pages/MappingSearch/InfoCard/index.js
+++ client/src/Pages/MappingSearch/InfoCard/index.js
@@ -1,28 +1,25 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 import Description from "./description";
 
-
 const Container = styled.div`
-    width: 344px;
-    height: 199px;
-    opacity: 0.1;
-    background-color: #fffefb;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: flex-start;
-    padding-bottom: 30px;
+  width: 344px;
+  height: 199px;
+  
+  background-color: #fffefb;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding-bottom: 30px;
 `;
 
-export const InfoCard = ({
-    ...props
-}) => {
-    return (
-        <Container>
-
-        </Container>
-    )
-}
+export const InfoCard = ({ ...props }) => {
+  return (
+    <Container>
+      <Description />
+    </Container>
+  );
+};
 
 export default InfoCard;
