83a216184a1633274da37871fa04a2616cad883e
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-07-02 20:11:09 -0300

    ♻️ Extract jobsInput into own component

---
+++ client/src/Components/JobsInput/index.js
@@ -0,0 +1,28 @@
+import React, { useCallback, useState, useEffect } from 'react'
+import Display from './Display'
+
+export const JobsInput = ({
+  onJobsChange = () => {},
+  ...props
+}) => {
+  const [ jobs, setJobs ] = useState([])
+
+  // update parent when jobs change
+  useEffect(() => { onJobsChange(jobs) }, [ jobs, onJobsChange ])
+
+  const onDelete = useCallback(chipToDelete => {
+    setJobs(chips => chips.filter(chip => chip.key !== chipToDelete.key))
+  }, [setJobs])
+
+  const onChange = useCallback(chip => {
+    if (jobs.length !== 6) setJobs(chip)
+  }, [jobs, setJobs])
+
+  return (
+    <Display
+      {...{ onChange, onDelete } }
+    />
+  )
+}
+
+export default JobsInput
