4186fd34b8bc7c4488f0a448bb7ceca8e0ee4405
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-07-02 14:52:32 -0300

    ✨ Add popup to blog#index

--- client/src/Pages/BlogList/index.js
+++ client/src/Pages/BlogList/index.js
@@ -1,14 +1,28 @@
-import React from "react";
-import Display from "./display";
-import { listAll } from "Api/blogs";
+import React, { useCallback } from "react";
 import { useAPICache } from "Hooks/useAPICache";
+
+import { listAll } from "Api/blogs";
+
+import PopUp from "Components/PopUp";
+import Display from "./display";
 
 export const Blog = ({ ...props }) => {
   const { data, status } = useAPICache(`blog_list`, [], listAll)
-  
-  if (status !== 200) console.log("Error in blog list page: ", status)
 
-  return <Display postList={data} />;
+  // error handling
+  const open = status !== 200
+  const { title, message } =
+    status === 404 ? { title: "Erro de conexão", message: "Por favor tente de novo mais tarde" } :
+    status === 500 ? { title: "Erro de conexão", message: "Por favor tente de novo mais tarde" } :
+    { title: "Erro", message: "Houve um erro. Tente de novo mais tarde" }
+
+  const onClose = useCallback(() => {
+  }, [])
+  
+  return (<>
+    <Display postList={data} />
+    <PopUp {...{ open, title, message, onClose }} />
+  </>)
 };
 
 export default Blog;
