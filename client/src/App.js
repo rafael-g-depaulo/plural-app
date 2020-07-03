5bfe4e06cac5a7e51842bdaef2252f85bade24f1
Raphael Müller <raphaelplmuller@icloud.com>
2020-07-03 10:52:31 -0300

    Move UserProvider to Routes.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,13 @@ import React from "react";
 import GlobalStyles from "GlobalStyles";
 import Fonts from "Fonts";
 import Routes from "Routes";
-import { UserProvider } from "Context/User/Provider";
 
 const App = () => {
   return (
     <>
       <Fonts />
       <GlobalStyles />
-      <UserProvider>
-        <Routes />
-      </UserProvider>
+      <Routes />
     </>
   );
 };
--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,95 +1,114 @@
 import React, { lazy, useContext } from "react";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import AsyncComponent from "Components/AsyncComponent";
 import UserContext from "Context/User";
+import { UserProvider } from "Context/User/Provider";
 
 const PasswordReset = lazy(() => import("Pages/PasswordReset"));
-const Confirmation = lazy(() => import('Pages/Confirmation'))
-const SignUp = lazy(() => import("Pages/SignUp"))
-const Login = lazy(() => import("Pages/Login"))
-const AreYou = lazy(() => import("Pages/AreYouLGBTQIA"))
-const MappingQuestion = lazy(() => import("Pages/MappingQuestion"))
-const BlogList = lazy(() => import("Pages/BlogList"))
-const Blog = lazy(() => import("Pages/Blog"))
-const Event = lazy(() => import("Pages/Event"))
-const SingUpMapping = lazy(() => import("Pages/Mapping"))
-const Programming = lazy(() => import("Pages/Programming"))
+const Confirmation = lazy(() => import("Pages/Confirmation"));
+const SignUp = lazy(() => import("Pages/SignUp"));
+const Login = lazy(() => import("Pages/Login"));
+const AreYou = lazy(() => import("Pages/AreYouLGBTQIA"));
+const MappingQuestion = lazy(() => import("Pages/MappingQuestion"));
+const BlogList = lazy(() => import("Pages/BlogList"));
+const Blog = lazy(() => import("Pages/Blog"));
+const Event = lazy(() => import("Pages/Event"));
+const SingUpMapping = lazy(() => import("Pages/Mapping"));
+const Programming = lazy(() => import("Pages/Programming"));
+
+const VerifyUser = ({ ...props }) => {
+  const user = useContext(UserContext);
+
+  return (
+    <Route exact path="/">
+      {user === null ? <Redirect to="/login" /> : <Redirect to="/event" />}
+    </Route>
+  );
+};
 
 export const Routes = ({ ...props }) => {
-  const user = useContext(UserContext)
-  
+  const user = useContext(UserContext);
+
+  console.log("USER CONTEXT", user);
+
   return (
     <Router basename="/">
-      <Switch>
-        {/* página de login */}
-        <Route path="/login">
-          <AsyncComponent>
-            <Login />
-          </AsyncComponent>
-        </Route>
+      <UserProvider>
+        <Switch>
+          {/* página de login */}
+          <Route path="/login">
+            <AsyncComponent>
+              <Login />
+            </AsyncComponent>
+          </Route>
 
-        {/* página de cadastro */}
-        <Route path="/signup">
-          <AsyncComponent>
-            <SignUp />
-          </AsyncComponent>
-        </Route>
+          {/* página de cadastro */}
+          <Route path="/signup">
+            <AsyncComponent>
+              <SignUp />
+            </AsyncComponent>
+          </Route>
 
-        {/* página da pergunta se um usuário é LGBTQ+ */}
-        <Route path="/areyouLGBTQIA">
-          <AsyncComponent>
-            <AreYou />
-          </AsyncComponent>
-        </Route>
+          {/* página da pergunta se um usuário é LGBTQ+ */}
+          <Route path="/areyouLGBTQIA">
+            <AsyncComponent>
+              <AreYou />
+            </AsyncComponent>
+          </Route>
 
-        {/* página que pergunta se um usuário quer participar do mapeamento */}
-        <Route path="/participar-mapeamento">
-          <AsyncComponent>
-            <MappingQuestion />
-          </AsyncComponent>
-        </Route>
-        
-        {/* página de um blogpost */}
-        <Route path="/blog/:id_post">        
-          <AsyncComponent>
-            <Blog />
-          </AsyncComponent> 
-        </Route>
+          {/* página que pergunta se um usuário quer participar do mapeamento */}
+          <Route path="/participar-mapeamento">
+            <AsyncComponent>
+              <MappingQuestion />
+            </AsyncComponent>
+          </Route>
 
-        {/* página de listagem dos blogposts */}
-        <Route path="/blog">        
-          <AsyncComponent>
-            <BlogList />
-          </AsyncComponent>
-        </Route>
+          {/* página de um blogpost */}
+          <Route path="/blog/:id_post">
+            <AsyncComponent>
+              <Blog />
+            </AsyncComponent>
+          </Route>
 
-        {/* página de um evento individual */}
-        <Route path="/event/:id_event">        
-          <AsyncComponent>
-            <Event />
-          </AsyncComponent>
-        </Route>
+          {/* página de listagem dos blogposts */}
+          <Route path="/blog">
+            <AsyncComponent>
+              <BlogList />
+            </AsyncComponent>
+          </Route>
 
-        {/* página que pergunta se um usuário quer participar do mapeamento */}
-        <Route path="/mapping">
-          <AsyncComponent>
-            <SingUpMapping />
-          </AsyncComponent>
-        </Route>
+          {/* página de um evento individual */}
+          <Route path="/event/:id_event">
+            <AsyncComponent>
+              <Event />
+            </AsyncComponent>
+          </Route>
 
-        {/* página de listagem da programação */}
-        <Route path="/event">
-          <AsyncComponent>
-            <Programming />
-          </AsyncComponent>
-        </Route>
+          {/* página que pergunta se um usuário quer participar do mapeamento */}
+          <Route path="/mapping">
+            <AsyncComponent>
+              <SingUpMapping />
+            </AsyncComponent>
+          </Route>
 
-        {/* página de aviso da confirmação de email */}
-        <Route path="/confirmation">
-          <AsyncComponent>
-            <Confirmation />
-          </AsyncComponent>
-        </Route>
+          {/* página de listagem da programação */}
+          <Route path="/event">
+            <AsyncComponent>
+              <Programming />
+            </AsyncComponent>
+          </Route>
+
+          {/* página de aviso da confirmação de email */}
+          <Route path="/confirmation">
+            <AsyncComponent>
+              <Confirmation />
+            </AsyncComponent>
+          </Route>
 
           {/* página de reset de senha */}
           <Route path="/password-reset/:token?">
@@ -98,15 +117,11 @@ export const Routes = ({ ...props }) => {
             </AsyncComponent>
           </Route>
 
-        {/* Home page */}
-        <Route exact path="/">
-          { user === null
-            ? <Redirect to="/login"/>
-            : <Redirect to="/event"/>
-          }
-        </Route>
-        
-      </Switch>
+          {/* Home page */}
+          <VerifyUser>
+          </VerifyUser>
+        </Switch>
+      </UserProvider>
     </Router>
   );
 };
