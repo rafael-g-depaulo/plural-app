a37ab3ddadfd2250f2d10fa6e391e2652d12a7d6
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-08-08 21:15:27 -0300

    temp logs

--- server/Middlewares/redirectToStrapi.js
+++ server/Middlewares/redirectToStrapi.js
@@ -1,5 +1,7 @@
 export const redirectToStrapi = (pathTransform = a => a) => (req, res, next) => {
 
+  console.log("ATEEMPTING TO REDIRECT:", req.originalUrl)
+
   // setup domain to redirect request to
   const domain = process.env.NODE_ENV === "production"
     ? "https://plural-server.herokuapp.com"
@@ -10,6 +12,7 @@ export const redirectToStrapi = (pathTransform = a => a) => (req, res, next) => 
   const path = /^\/api(\/.*)$/.exec(req.originalUrl)[1]
   const redirectUrl = `${domain}${pathTransform(path)}`
 
+  console.log("REDIRECTING TO", redirectUrl)
   // redirect to Strapi
   res.redirect(redirectUrl)
 }
