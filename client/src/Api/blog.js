7f27bb1a03690a6ac8094372b1ec1233f61572f0
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-08-08 21:26:19 -0300

    temp logs

--- client/src/Api/blog.js
+++ client/src/Api/blog.js
@@ -3,6 +3,10 @@ import api from "Api"
 export const getBlogPost = post_id => api
   .get(`/api/blogs/${post_id}`, { withCredentials: false })
   
-export const listAll = () => api
-  .get("/api/blog",  { withCredentials: false })
+export const listAll = () => Promise.resolve()
+  .then(() => {
+    console.log("ABOUT TO CALL API FOR BLOGS")
+    return api.get("/api/blog",  { withCredentials: false })
+  })
+  .then((a) => { console.log("RESOLVED WITH", a); return a})
   
